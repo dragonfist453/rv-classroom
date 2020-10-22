b6c8db8fec290a098f8f484d162fe483c7c9bf32
dragonfist453 <thermosphere453@gmail.com>
2020-10-22 16:16:25 +0530

    Updates

---
+++ precache-manifest.9ef36da863848aeba6b2661190aa3f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afebac1b4c487c4a44764cd639a5170",
+    "url": "/rv-classroom/index.html"
+  },
+  {
+    "revision": "9ff7eaaaea25d29d4b86",
+    "url": "/rv-classroom/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "9628c9fc4d001751ec93",
+    "url": "/rv-classroom/static/js/2.45e8e3ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rv-classroom/static/js/2.45e8e3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff7eaaaea25d29d4b86",
+    "url": "/rv-classroom/static/js/main.085b32e1.chunk.js"
+  },
+  {
+    "revision": "f80ab7074f037a7878c4",
+    "url": "/rv-classroom/static/js/runtime-main.b2b607e3.js"
+  }
+]);
